b74c9288e5ba761be19c620123cf936ef12044d4
Prithvi singh <prithvi.singh@1mg.com>
2022-12-31 19:44:33 +0530

    Initialized project

---
+++ src/test/java/dev/prithvis/blogbackend/BlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prithvis.blogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.ResultActions;
+
+@SpringBootTest
+class BlogBackendApplicationTests {
+    @Test
+    void contextLoads() {
+
+    }
+}
