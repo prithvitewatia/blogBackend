d41dde7d5bb20b6c05d55d7923100206785f5f6e
Prithvi singh <prithvi.singh@1mg.com>
2023-01-06 09:24:50 +0530

    Created Blog User service interface

---
+++ src/main/java/dev/prithvis/blogbackend/service/repo/BlogUserService.java
@@ -0,0 +1,17 @@
+package dev.prithvis.blogbackend.service.repo;
+
+import dev.prithvis.blogbackend.payloads.BlogUserDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface BlogUserService {
+    BlogUserDTO getUserById(Integer id);
+
+    Page<BlogUserDTO> getUsers(Pageable pageable);
+
+    BlogUserDTO createUser(BlogUserDTO userDTO);
+
+    BlogUserDTO updateUser(BlogUserDTO userDTO, Integer userId);
+
+    void deleteUser(Integer id);
+}
