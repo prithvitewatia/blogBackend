694e7eff757ff2d8c476831b7a9cf8859db7663a
Prithvi singh <prithvi.singh@1mg.com>
2023-01-12 20:39:18 +0530

    Added exception handler for method argument not valid exception

--- src/main/java/dev/prithvis/blogbackend/exceptions/ApiExceptionHandler.java
+++ src/main/java/dev/prithvis/blogbackend/exceptions/ApiExceptionHandler.java
@@ -2,11 +2,14 @@ package dev.prithvis.blogbackend.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -18,5 +21,21 @@ public class ApiExceptionHandler {
                 "path", ((ServletWebRequest) webRequest).getRequest().getRequestURI(),
                 "success", "false"),
                 HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
+                                                                                    WebRequest webRequest){
+        Map<String,String> errorMessages=new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(objectError -> {
+            String fieldName=((FieldError)objectError).getField();
+            errorMessages.put(fieldName,
+                    objectError.getDefaultMessage());
+        });
+        return new ResponseEntity<>(Map.of(
+                "path",((ServletWebRequest)webRequest).getRequest().getRequestURI(),
+                "success",false,
+                "count",errorMessages.size(),
+                "errors",errorMessages
+        ),HttpStatus.BAD_REQUEST);
     }
 }
